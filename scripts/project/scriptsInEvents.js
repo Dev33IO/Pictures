3ab19627b0af2acd9415154055bafcc965f9523f
Dev33IO <127096734+Dev33IO@users.noreply.github.com>
2023-06-15 13:21:07 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,29 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event4_Act4(runtime, localVars)
+	{
+		localStorage.setItem('color', "#0000FF");
+	},
+
+	async Egame_Event5_Act4(runtime, localVars)
+	{
+		localStorage.setItem('color', "#FF0000");
+	},
+
+	async Egame_Event6_Act4(runtime, localVars)
+	{
+		localStorage.setItem('color', "#FFFF00");
+	},
+
+	async Egame_Event7_Act4(runtime, localVars)
+	{
+		localStorage.setItem('color', "#FFFFFF");
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
